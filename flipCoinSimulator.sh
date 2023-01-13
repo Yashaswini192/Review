ebea005854ac05abf04b8d37aba84464b4d1ac2d
Yashaswini192 <121873686+Yashaswini192@users.noreply.github.com>
2023-01-13 21:42:43 +0530

    Update flipCoinSimulator.sh

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -3,9 +3,17 @@
 
 FLIP_COIN=$(($((RANDOM%10))%2))
 
-if [ $FLIP_COIN -eq 1 ]
-then
-	echo " HEADS";
-else 
-	echo "TAILS";
-fi
+for ((i=0;i<=21;++i))
+do
+	if [ $FLIP_COIN -eq 1 ]
+	then
+		echo " HEADS";
+	else 
+		echo "TAILS";
+	fi
+done
+
+echo "FLIPCOIN:" ${FLIP_COIN[@]};
+echo "HEAD/TAIL:"${!FLIP_COIN[@]};
+
+
